64592aa8090f0f8205daf7f71e7bee474ac46f57
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 19:33:24 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,3 +7,8 @@ game "rdr3"
 client_script {'config.lua','client.lua','warmenu.lua'}
 server_script {'config.lua', 'server.lua'}
 
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/Vorp_walkanim'
