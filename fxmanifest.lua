ffa6b28cb45845a0d3d74c27ae83397c3ef197b8
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-06-05 14:55:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'VORP edit by @outsider'
+author 'blue edit by @outsider'
 game "rdr3"
 lua54 'yes'
 
